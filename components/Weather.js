f9a3bbb9bc0c15670324b84a90c78f60cc4928db
Nuttanai Yaprajan <nuttanai03970@gmail.com>
2021-07-15 11:38:25 +0700

    fetch works

--- components/Weather.js
+++ components/Weather.js
@@ -1,7 +1,6 @@
-import React, { useState } from "react";
+import React, { useState , useEffect} from "react";
 import { StyleSheet, Text, ImageBackground, View } from "react-native";
 import Forecast from "./Forcast";
-
 export default function Weather(props) {
     const [forecastInfo, setForecastInfo] = useState({
         main: '-',
@@ -9,12 +8,31 @@ export default function Weather(props) {
         temp: 0
     })
 
+    useEffect(() => {
+        console.log(`fetching data with zipCode = ${props.zipCode}`)
+        if (props.zipCode) {
+            fetch(`http://api.openweathermap.org/data/2.5/weather?q=${props.zipCode},th&units=metric&appid=16749032afae50b610388ee198465e8d`)
+                .then((response) => response.json())
+                .then((json) => {
+                    setForecastInfo({
+                        main: json.weather[0].main,
+                        description: json.weather[0].description,
+                        temp: json.main.temp
+                    });
+                })
+                .catch((error) => {
+                    console.warn(error);
+                });
+        }
+    }, [props.zipCode])
+
+
     return (
         <View>
             <ImageBackground source={require('../bg.png')} style={styles.backdrop}>
                 <View style={styles.container}>
                     <Text style={styles.text}>Zip Code is {props.zipCode}</Text>
-                    
+
                     <Forecast {...forecastInfo} />
                 </View>
 
@@ -31,14 +49,14 @@ const styles = StyleSheet.create({
     },
     container: {
         padding: 20,
-        width:'100%',
-        alignItems:'center',
-        backgroundColor:'#000',
+        width: '100%',
+        alignItems: 'center',
+        backgroundColor: '#000',
         opacity: 0.5,
-        
+
     },
-    text:{
-        fontSize:24,
-        color:'#fff',
+    text: {
+        fontSize: 24,
+        color: '#fff',
     }
 });
