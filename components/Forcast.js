e5b9894a47517c9db180fb9b2924095f1da5b9d6
Nuttanai Yaprajan <nuttanai03970@gmail.com>
2021-07-15 10:57:16 +0700

    adjust forcase style

--- components/Forcast.js
+++ components/Forcast.js
@@ -1,10 +1,12 @@
 import React from "react";
-import { Text,View } from "react-native";
+import {StyleSheet ,Text,View } from "react-native";
 
 export default function Forecast(props) {
     return (
         <View >
-            <Text>{props.main}</Text>
+            <Text>main</Text>
+            <Text style>{props.main}</Text>
+            <Text>description</Text>
             <Text>{props.description}</Text>
             <View>
                 <Text>{props.temp}</Text>
@@ -13,3 +15,13 @@ export default function Forecast(props) {
         </View >
     );
 } 
+
+const styles = StyleSheet.create({
+    text:{
+        fontSize:24,
+        color:'#fff',
+    },
+    description:{
+        fontSize:18,
+    }
+});
